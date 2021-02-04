8953e3f12fed1cdc39cbb704408455d31e37eee9
user-juli <julissaortiz@marketingmsd.com>
2021-02-04 09:21:54 -0500

    my first django-site

---
+++ static/js/main.js
@@ -0,0 +1,124 @@
+//------ DROPDOWN - GUÍA TURÍSTICA ------ //
+const btnGuia = document.getElementById('btn-guia'),
+	  btnCerrarMenu = document.getElementById('btn-menu-cerrar'),
+	  grid = document.getElementById('grid'),
+	  contenedorEnlacesNav = document.querySelector('#menu .contenedor-enlaces-nav'),
+	  contenedorSubCategorias = document.querySelector('#grid .contenedor-subcategorias'),
+	  esDispositivoMovil = () => window.innerWidth <= 800;
+
+btnGuia.addEventListener('mouseover', () => {
+	if(!esDispositivoMovil()){
+		grid.classList.add('activo');
+	}
+});
+
+grid.addEventListener('mouseleave', () => {
+	if(!esDispositivoMovil()){
+		grid.classList.remove('activo');
+	}
+});
+
+document.querySelectorAll('#menu .categorias a').forEach((elemento) => {
+	elemento.addEventListener('mouseenter', (e) => {
+		if(!esDispositivoMovil()){
+			document.querySelectorAll('#menu .subcategoria').forEach((categoria) => {
+				categoria.classList.remove('activo');
+				if(categoria.dataset.categoria == e.target.dataset.categoria){
+					categoria.classList.add('activo');
+				}
+			});
+		};
+	});
+});
+
+// EventListeners para dispositivo movil.
+document.querySelector('#btn-menu-barras').addEventListener('click', (e) => {
+	e.preventDefault();
+	if(contenedorEnlacesNav.classList.contains('activo')){
+		contenedorEnlacesNav.classList.remove('activo');
+		document.querySelector('body').style.overflow = 'visible';
+	} else {
+		contenedorEnlacesNav.classList.add('activo');
+		document.querySelector('body').style.overflow = 'hidden';
+	}
+});
+
+// Click en boton de todos los departamentos (Para version movil).
+btnGuia.addEventListener('click', (e) => {
+	e.preventDefault();
+	grid.classList.add('activo');
+	btnCerrarMenu.classList.add('activo');
+});
+
+// Boton de regresar en el menu de categorias
+document.querySelector('#grid .categorias .btn-regresar').addEventListener('click', (e) => {
+	e.preventDefault();
+	grid.classList.remove('activo');
+	btnCerrarMenu.classList.remove('activo');
+});
+
+document.querySelectorAll('#menu .categorias a').forEach((elemento) => {
+	elemento.addEventListener('click', (e) => {
+		if(esDispositivoMovil()){
+			contenedorSubCategorias.classList.add('activo');
+			document.querySelectorAll('#menu .subcategoria').forEach((categoria) => {
+				categoria.classList.remove('activo');
+				if(categoria.dataset.categoria == e.target.dataset.categoria){
+					categoria.classList.add('activo');
+				}
+			});
+		}
+	});
+});
+
+// Boton de regresar en el menu de categorias
+document.querySelectorAll('#grid .contenedor-subcategorias .btn-regresar').forEach((boton) => {
+	boton.addEventListener('click', (e) => {
+		e.preventDefault();
+		contenedorSubCategorias.classList.remove('activo');
+	});
+});
+
+btnCerrarMenu.addEventListener('click', (e)=> {
+	e.preventDefault();
+	document.querySelectorAll('#menu .activo').forEach((elemento) => {
+		elemento.classList.remove('activo');
+	});
+	document.querySelector('body').style.overflow = 'visible';
+});
+
+
+//------ JQUERY ------ //
+
+jQuery(document).ready(function() {
+
+//-- ANIMACIONES CON SCROLL - AOS --//
+AOS.init();
+
+//------ CAROUSEL - ACTIVIDADES ------ //
+$('#carousel-example').on('slide.bs.carousel', function (e) {
+
+	/*
+					CC 2.0 License Iatek LLC 2018
+					Attribution required
+	*/
+	var $e = $(e.relatedTarget);
+	var idx = $e.index();
+	var itemsPerSlide = 4;
+	var totalItems = $('.carousel-item').length;
+
+	if (idx >= totalItems-(itemsPerSlide-1)) {
+					var it = itemsPerSlide - (totalItems - idx);
+					for (var i=0; i<it; i++) {
+									// append slides to end
+									if (e.direction=="left") {
+													$('.carousel-item').eq(i).appendTo('.carousel-inner');
+									}
+									else {
+													$('.carousel-item').eq(0).appendTo('.carousel-inner');
+									}
+					}
+	}
+});
+
+});
